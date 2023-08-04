ff5a604369cb60e8a453ea059f4ff33f45da41ac
shixingyu <32320811@qq.com>
2023-08-04 17:25:22 +0800

    新增配置项

---
+++ src/config/swoole.php
@@ -0,0 +1,12 @@
+<?php
+return [
+    'redis' => [
+        'host' => '127.0.0.1',
+        'port' => '6379',
+        'password' => '',
+    ],
+    'websocket' => [
+        'host' => '127.0.0.1',
+        'port' => '9502',
+    ],
+];
