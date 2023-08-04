20d9681c3790b478d4eec20aacf3eee80e131f2b
shixingyu <32320811@qq.com>
2023-08-04 18:29:20 +0800

    更改文件名

---
+++ src/app/console/swoole/baseController.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\swoole;
+
+use rephp\core\controller;
+
+/**
+ * 基类
+ * @package app\modules\index
+ */
+class baseController extends controller
+{
+}
---
+++ src/app/console/swoole/controller/websocketController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace app\modules\swoole\controller;
+
+use app\modules\swoole\logic\websocketLogic;
+use app\swoole\baseController;
+use rephp\swoole\websocket\websocket;
+
+class websocketController extends baseController
+{
+    public $layout = 'index';
+
+    /**
+     * 运行websocket
+     * @return void
+     */
+    public function runAction()
+    {
+        try {
+            $config = [
+                'websocket' => config('swoole.websocket'),
+                'redis'     => config('swoole.redis'),
+            ];
+
+            $onMessageEvent = websocketLogic::getOnMessageEvent();
+            $websocket      = new websocket($config, $onMessageEvent);
+            $websocket->getServer()->start();
+        } catch (\Exception $e) {
+            echo 'Error: ' . $e->getMessage() . "\n";
+            echo 'File: ' . $e->getFile() . ':' . $e->getLine() . "\n";
+            echo 'Trace: ' . $e->getTraceAsString() . "\n";
+            exit('');
+        }
+
+    }
+}
